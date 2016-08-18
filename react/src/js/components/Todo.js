34483906e7751a546239f26bc89cfce91f4c1f1b
Christoph Walpert <cwalpert@yahoo.com>
2016-08-18 14:44:24 +0200

    finished the todo component

--- react/src/js/components/Todo.js
+++ react/src/js/components/Todo.js
@@ -10,10 +10,19 @@ export default class Todo extends React.Component {
 
     const icon = complete ? "\u2714" : "\u2716"
 
-    
+    if (edit) {
+      return (
+        <li>
+          <input value={text} focus="focused"/>
+        </li>
+      );
+    }
 
     return (
-
+      <li>
+        <span>{text}</span>
+        <span>{icon}</span>
+      </li>
     )
   }
 }
